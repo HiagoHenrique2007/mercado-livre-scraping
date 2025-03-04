72f628990dd3e1b3a4482edb7a59606d0617992b
hiago <imhiagohenrique@gmail.com>
2025-03-04 12:38:03 -0300

    ...

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 from scraping.scraper import scraper
 
 app = Flask(__name__)
 
-@app.route('/api/categories')
+@app.route('/')
 def returnCategories():
   categories = scraper.getCategories()
-  return categories
+  product = scraper.getDatasProduct('https://www.mercadolivre.com.br/teclado-soft-silence-preto-com-fio-teclas-baixas-multilaser-idioma-portugus-brasil/p/MLB22833016#polycard_client=recommendations_home_navigation-trend-recommendations&reco_backend=machinalis-homes-univb&wid=MLB3815285341&reco_client=home_navigation-trend-recommendations&reco_item_pos=2&reco_backend_type=function&reco_id=f4ab0ad6-1c7f-4e9d-be6c-b2cacca4c2ed&sid=recos&c_id=/home/navigation-trend-recommendations/element&c_uid=bfc46f02-e1fe-4150-80a8-4eff93b534f5')
+  if categories['sucess'] == True and product['sucess'] == True:
+    return render_template('index.html', categories=categories, result=product)
 
 app.run(debug=True)
--- scraping/scraper.py
+++ scraping/scraper.py
@@ -6,6 +6,11 @@ class Scraper:
   home_page = 'https://www.mercadolivre.com.br/'
   categories_ul_class = 'nav-categs-departments'
 
+
+  sucess_false =  {
+    'sucess': False,
+  }
+
   def getHtml(self, url):
     try:
       response = requests.get(url, timeout=5, allow_redirects=False)
@@ -23,17 +28,49 @@ class Scraper:
   def getCategories(self):
     html = self.getHtml(self.home_page)
     if html is None:
-      return None
+      return 
     
     soup = Soup(html, 'html.parser')
     categories_list = soup.find('ul', class_=self.categories_ul_class)
     if categories_list is None:
       print('Nao foi possivel obter as categorias!')
-      return None
+      return self.sucess_false
     
     categories_list = categories_list.find_all('li')    
     categories = [ category.text.strip() for category in categories_list ]
     categories.pop()
-    return categories
+    return {
+      'sucess': True,
+      'categories': categories
+    }
   
+  def getLinkProduct(self, url)
+
+  def getDatasProduct(self, url):
+    html = self.getHtml(url)
+    soup = Soup(html, 'html.parser')
+    product_name = soup.find('h1')
+    price = soup.find('meta', itemprop='price')
+    rating = soup.find('span', class_='ui-pdp-review__rating')
+    if product_name and price and rating:
+      product = {
+        'name': product_name.text,
+        'price': price['content'],
+        'rating': rating.text
+      }
+      return {
+        'sucess': True,
+        'product': product
+      }
+    
+    else:
+      return self.sucess_false
+
+    
+
+
+
+
+
+
 scraper = Scraper()
